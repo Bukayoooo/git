3d3204c97db0812af06edc1923de4eb23aa3d2a9
Bukayoooo <67252967+Bukayoooo@users.noreply.github.com>
2023-05-10 22:23:14 +0800

    add func1

--- hello.c
+++ hello.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+void func1() {
+    printf("func1 written on windows!");
+}
+
 int main() {
 //<<<<<<< HEAD
     printf("hello git in master!\n");
 //=======
     printf("hello git in dev!\n");
 //>>>>>>> dev
+
+    func1();
+
     return 0;
 }
